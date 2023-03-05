077ee6b6481835103451d76810b733710f99b5af
jdiego2212 <juan444622@outlook.com>
2023-03-05 17:57:04 +0100

    Calculator

---
+++ Ejerc-1/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', () => {
+    
+    const display = document.querySelector('#display');
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((item) => {
+        item.onclick = ()=> {
+            if (item.id === 'clear') {
+                display.innerText = "";
+
+            } else if (item.id === 'backspace') {
+                let string = display.innerText.toString();
+                display.innerText = string.substr(0, string.length - 1);
+
+            } else if (display.innerText != "" && item.id === "equal") {
+                display.innerText = eval(display.innerText);
+
+            } else if (display.innerText === "" && item.id === "equal") {
+                display.innerText = "Null";
+                setTimeout(() => (display.innerText = ""), 2000);
+
+            } else {
+                display.innerText += item.innerText;
+
+            }
+        };
+    });
+
+    const themeTogglerBtn = document.querySelector('.theme-toggler');
+    const calculator = document.querySelector('.calculator');
+    const togglerIcon = document.querySelector('.toggler-icon');
+
+    let isDark = true;
+
+    themeTogglerBtn.onclick = ()=> {
+        calculator.classList.toggle('dark');
+        themeTogglerBtn.classList.toggle('active');
+        isDark = !isDark;
+    }
+
+})
