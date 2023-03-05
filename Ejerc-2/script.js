041dc6dd33bbd8f326dc7ddc0b12a5f87d1770d9
jdiego2212 <juan444622@outlook.com>
2023-03-05 20:28:33 +0100

    Menú finshed

--- Ejerc-2/script.js
+++ Ejerc-2/script.js
@@ -1,4 +1,22 @@
 window.addEventListener('load', () => {
 
     const menuToggle = document.querySelector('.menu-toggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.onclick = () => {
+        navigation.classList.toggle('open');
+    };
+
+    const list = document.querySelectorAll('.list');
+
+    function activeLink() {
+        list.forEach((item) => {
+            item.classList.remove('active');
+            this.classList.add('active');
+        });
+    };
+    list.forEach((item) => {
+        item.addEventListener('click', activeLink);
+    });
+
 });
