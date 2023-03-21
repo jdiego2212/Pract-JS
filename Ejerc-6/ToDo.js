de3fcf769adb06993987390067beb4dab2a16079
jdiego2212 <juan444622@outlook.com>
2023-03-21 11:31:45 +0100

    JS con Error

--- Ejerc-6/ToDo.js
+++ Ejerc-6/ToDo.js
@@ -5,6 +5,7 @@ window.addEventListener( 'load', () => {
     const month = document.querySelector('.month')
     const year = document.querySelector('.year')
 
+
     // Toggle Theme
     const toggleTheme = document.querySelector('.toggle')
     const body = document.querySelector('body')
@@ -14,16 +15,22 @@ window.addEventListener( 'load', () => {
     const input = document.querySelector('#content .add-task input[type="text"]')
     const btnAdd = document.querySelector('#content .add-task button')
     const gitIcon = document.querySelector('footer img')
-    const textP = document.querySelectorAll('list > li > p')
+
 
     // Add Tasks
     const list = document.querySelector('#list')
-    let id = 0
+    let id
+
 
     // Check Button
     const checkedCl = 'line-through'
-    const check = 'check_circle'
-    const uncheck = 'radio_button_unchecked'
+    const check = 'fa-check-circle'
+    const uncheck = 'fa-circle'
+
+
+    // Local Storage
+    let LIST
+
 
     // Set Date Function
     function setDate () {
@@ -34,6 +41,7 @@ window.addEventListener( 'load', () => {
     }
 
     setDate()
+
 
     // Change Theme Function
     function changeTheme () {
@@ -48,12 +56,11 @@ window.addEventListener( 'load', () => {
         input.classList.toggle('theme-dark')
         btnAdd.classList.toggle('theme-dark')
         gitIcon.classList.toggle('theme-dark')
-        textP.forEach(p => {
-            p.classList.toggle('theme-dark')
-        })
+        list.parentNode.querySelector('.text').classList.toggle('theme-dark')
     }
 
     toggleTheme.addEventListener('click', changeTheme)
+
 
     // Add Task Function
     function addTask (task, id, done, deleted) {
@@ -64,50 +71,75 @@ window.addEventListener( 'load', () => {
 
         const taskHtml = `
             <li id="${ id }">
-                <span id="check" class="material-symbols-outlined" data="done">${ checked }</span> <p class="${ line }">${ task }</p> <span id="delete" class="material-symbols-outlined" data="deleted">delete</span>
+                <span id="check" class="far ${ checked }" data="done"></span> <p class="text ${ line }">${ task }</p> <span id="delete" class="fas fa-trash de" data="deleted"></span>
             </li>
         `
         list.insertAdjacentHTML('beforeend', taskHtml)
     }
 
+
     //Task Done Function
     function taskDone (element) {
         element.classList.toggle(check)
         element.classList.toggle(uncheck)
+        element.parentNode.querySelector('.text').classList.toggle(checkedCl)
+        LIST[element.id].done = LIST[element.id].done ?false :true
     }
+
 
     // Task Deleted Function
     function taskDeleted (element) {
-        
+        element.parentNode.parentNode.removeChild(element.parentNode)
+        LIST[element.id].deleted = true
     }
 
+
+    // Event to Add The Task
     btnAdd.addEventListener('click', () => {
         const task = input.value
         if (task) {
             addTask(task, id, false, false)
+            LIST.push({
+                name : task,
+                id : id,
+                done : false,
+                deleted : false
+            })
         } else {
             alert('Ingrese una tarea')
             return
         }
-        input.value = ''
+        localStorage.setItem('ToDo',JSON.stringify(LIST))
         id ++
+        input.value = ''
     })
 
+
+    // Event to Add the Task with the key Enter
     document.addEventListener('keyup', (e) => {
         if (e.key === 'Enter') {
             const task = input.value
             if (task) {
                 addTask(task, id, false, false)
+                LIST.push({
+                    name : task,
+                    id : id,
+                    done : false,
+                    deleted : false
+                })
             } else {
                 alert('Ingrese una tarea')
                 return
             }
-            input.value = ''
+            localStorage.setItem('ToDo',JSON.stringify(LIST))
             id ++
+            input.value = ''
         }
         
     })
 
+
+    // Event to Check and to Delete the Task
     list.addEventListener('click', (event) => {
         const element = event.target 
         const elementData = element.attributes.data.value
@@ -118,6 +150,25 @@ window.addEventListener( 'load', () => {
         } else if (elementData === 'deleted') {
             taskDeleted(element)
         }
-
+        localStorage.setItem('ToDo',JSON.stringify(LIST))
     })
+
+    
+    //LocalStorage
+    function loadList (DATA) {
+        DATA.forEach(element => {
+            addTask(element.name, element.id, element.done, element.deleted)
+        })
+    }
+
+    let data = localStorage.getItem('ToDo')
+
+    if (data) {
+        LIST = JSON.parse(data)
+        id = LIST.length
+        loadList(LIST)
+    } else {
+        LIST = []
+        id = 0
+    }
 })
